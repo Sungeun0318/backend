43bd7bd139dbd3c9478a5afed1736c4d0f867b2a
Sungeun0318 <kimsungeun0318@naver.com>
2026-02-04 09:43:42 +0900

    datebase practice2

---
+++ src/database/day02/exam1.sql
@@ -0,0 +1,90 @@
+create database mydb0203; -- db생성
+use mydb0203; -- db 활성화/선택/사용 
+
+-- [1] 테이블 생성
+-- create table 테이블명(속성명 타입 제약조건, 속성명 타입 제약조건);
+create table test1(속성명 int);
+
+-- [2] 테이블 목록 보기, show tables;
+show tables;
+-- 2개 이상의 속성명 정의한 테이블
+create table test2(속성명1 int, 속성명2 double);
+-- [3] 테이블 삭제, drop table 테이블명;, drop/database if exists 테이블/데이터베이스명
+drop table if exists test1;
+-- [4] 테이블 간단 조회, select 속성명1, 속성명2 from 테이블명;, select*from 테이블명;
+select * from test2;
+-- [5] 다양한 데이터타입의 테이블 생성
+create table test3(
+	정수속성1 tinyint, 정수속성2 smallint, 정수속성3 mediumint,
+    정수속성4 int, 정수속성5 bigint, 정수속성6 int unsigned, -- signed 부호있는(+-) unsigned(부호없음)
+    실수속성1 float, 실수속성2 double, 실수속성3 decimal, -- decimal 문자타입으로 실수표현(오차x/부동소수점x)
+    날짜속성1 date, 시간속성2 time, 날짜시간속성3 datetime,
+    문자속성1 char(5), -- 고정길이의 문자타입, char(5) -> "유재석" -> [유][재][석][] [] 2칸이 남는다.
+    문자속성2 varchar(5), -- 가변길이의 문자타입, varchar(5) -> "유재석" -> [유][재][석] x x 남은 2칸은 삭제한다.
+    문자속성3 text, 문자속성4 longtext, -- 4G 대용량
+    논리속성 bool -- true 또는 false, tinyint 취급, 1 또는 0
+    );
+    
+select * from test3;
+
+-- [*] 방문록 기록하는 테이블설계
+create table comment (
+	content varchar(255), -- 내용물 최대 255글자
+	writer varchar(20) -- 작성자 최대 20글자
+);
+select *from comment;
+-- [*] 대기명단 기록하는 테이블설계
+create table waiting(
+	count tinyint unsigned,  -- 인원수 최대 정수 255까지, signed +- 128
+    phone char(13) -- 연락처 최대 13글자까지 고정길이 문자
+);
+select * from waiting;
+
+
+-- [6] 제약조건 : 테이블내 데이터들의 문제와 결함이 되는 입력 방지/해결
+create table test4(
+	# 속성명 타입 제약조건명
+	속성명1 tinyint not null,       -- not null 해당 속성값들은 null 저장할 수 없다.
+	속성명2 smallint unique,        -- unique 해당 속성값들은 중복이 불가능하다.
+	속성명3 int default 10, 		  -- default 자료 : 만일 해당 속성에 초기값이 생략되면 기본값 자료 사용한다.
+	속성명4 bigint auto_increment,   -- auto_increment : 만일 해당 속성에 초기값이 생략되면 자동번호 부여한다. 1~
+    constraint primary key(속성명4)
+	-- 속성명5 mediumint primary key   -- primary key : pk(식별/기본)키, 식별가능한 고유한 값 가진 키
+								  -- 중복없음 unique + 빈칸없음not null, 학번, 사번, 주민등록번호, 제품번호 등등
+								  -- vs constraint primary key (속성명)
+);
+
+create table test5(
+	속성명1 bigint,
+	constraint foreign key(속성명1) references 테이블명 (속성명4) on delete restrict
+    -- foreign key : fk(참조/외래)키, 다른 테이블의 기본키(pk)참조하는 키
+	-- constraint foreign key(FK속성명) references 테이블명 (pk필드명) 
+    -- 중복가능, 빈칸가능 
+	-- pk(1) : fk(N) 관계에서 수강신청학번, 휴가신청학번사번, 구매한제품번호 등등
+    -- on update/delete restrict : fk와 pk 참조 관계일때 pk 삭제/수정 불가능 관계 < 자식이 있으면 부모 삭제 불가능 >, 기본값
+					-- cascade : pk가 삭제/수정 될 때 fk도 같이 삭제 관계 < 게시물삭제되면 댓글도 같이 삭 >
+					-- set null : pk가 삭제/수정 될 때 fk는 null 변경 관계, < 게시물삭제되면 댓글 참조는 null >
+    );
+    select * from test4;
+    select * from test5;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
